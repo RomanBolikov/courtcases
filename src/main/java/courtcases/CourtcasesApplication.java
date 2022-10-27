65b1d3c98ae0cdd94281263d18e6283b2da0b1ed
Боликов Роман Александрович <rabolikov@MStr.local>
2022-10-27 16:30:26 +1000

    Intermediate commit

--- src/main/java/courtcases/CourtcasesApplication.java
+++ src/main/java/courtcases/CourtcasesApplication.java
@@ -1,11 +1,11 @@
 package courtcases;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import javafx.application.Application;
 
-
-@SpringBootApplication
-public class CourtcasesApplication {
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+public class CourtCasesApplication {
 
 	public static void main(String[] args) {
 		Application.launch(JavaFxApplication.class, args);
--- src/main/java/courtcases/JavaFxApplication.java
+++ src/main/java/courtcases/JavaFxApplication.java
@@ -2,12 +2,12 @@ package courtcases;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import net.rgielen.fxweaver.core.FxWeaver;
 
 public class JavaFxApplication extends Application {
 	private ConfigurableApplicationContext appContext;
@@ -15,16 +15,17 @@ public class JavaFxApplication extends Application {
 	@Override
 	public void init() {
 		String[] args = getParameters().getRaw().toArray(new String[0]);
-		this.appContext = new SpringApplicationBuilder().sources(CourtcasesApplication.class).run(args);
+		this.appContext = new SpringApplicationBuilder().sources(CourtCasesApplication.class).run(args);
 	}
 
 	@Override
 	public void start(Stage stage) {
-		FxWeaver fxweaver = appContext.getBean(MyController.class);
-		Parent root = fxWeaver.loadview(MyController.class);
-		Scene scene = new Scene(root);
+		FxWeaver fxWeaver = appContext.getBean(FxWeaver.class);
+		Parent parent = fxWeaver.loadView(MainController.class);
+		Scene scene = new Scene(parent);
+		stage.setScene(scene);
+		stage.setTitle("Авторизация");
 		stage.show();
-
 	}
 
 	public void stop() {
